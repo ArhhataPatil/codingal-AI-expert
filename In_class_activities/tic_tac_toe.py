50921925f80d0b8d0e0c5a59267874894bda7bd6
ArhhataPatil <arhhatap@gmail.com>
2026-06-22 17:58:58 +1000

    python

--- In_class_activities/tic_tac_toe.py
+++ In_class_activities/tic_tac_toe.py
@@ -24,7 +24,54 @@ def display(board):
             print(f"{colored(board[3])} | {colored(board[4])} | {colored(board[5])}")
             print("--+---+--")
             print(f"{colored(board[6])} | {colored(board[7])} | {colored(board[8])}")
-            
+
+def player_move(board, player_symbol):
+     move=-1
+     while move not in range(1,9) or board[move-1].isdigit():
+          try:
+               move=int(input("Tnter yiur number (1-9):"))
+               if move not in range (1,10) or not board[move-1].isdigit():
+                    print("Invalid move. Please try again.")
+          except:
+               print("Invalid input. Please enter a number between 1 and 9.")
+     board[move-1]=player_symbol
+
+def Ai_move(board, ai_symbol,player_symbol):
+     for i in range (9):
+          if board[i].isdigit():
+               board_copy=board.copy()
+               board_copy[i]=player_symbol
+               if check_win(board_copy, player_symbol):
+                      board[i]=ai_symbol
+                      return
+     for i in range(9):
+          if board[i].isdigit():
+               board_copy=board.copy()
+               board_copy[i]=ai_symbol
+               if check_win(board_copy, ai_symbol):
+                    board[i]=ai_symbol
+                    return
+     possible_moves=[i for i in range(9) if board[i].isdigit()]
+     move=random.choice(possible_moves)
+     board[move]=ai_symbol
+
+def check_win(board, symbol):
+     win_conditions=[
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+]
+         for condition in win_conditions:
+            if board[condition[0]] == board[condition[1]] == board[condition[2]]==symbol:
+                    return True
+        return False
+                 
+
 
 def ttt():
     print(f"Welcome to Tic Tac Toe!")
@@ -37,8 +84,28 @@ def ttt():
 
         while game_on:
             display(board)
-            break
-        break
+            if turn=='player':
+                player_move(board, player_s)
+                if check_win(board, player_s):
+                    display(board)
+                    print(f"Congratulations {player_n}! You win!")
+                    game_on=False
+                else:
+                    if check_full(board):
+                        display(board)
+                        print("It's a tie!")
+                    else:
+                        turn='AI'
+
+                else:
+                    ai_move(board,ai_symbol, player_symbol)
+                    if check_win(board, ai_symbol):
+                        display(board)
+                        print(f"Sorry {player_n}, the AI wins!")
+                        game_on=False
+                else:
+            
+        
 
 if __name__=="__main__":
     ttt()
