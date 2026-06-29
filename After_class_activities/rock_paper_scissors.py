0fb8191b16b76ca56674640e1a3246c00d617dd2
ArhhataPatil <arhhatap@gmail.com>
2026-06-29 15:12:03 +1000

    python

--- After_class_activities/rock_paper_scissors.py
+++ After_class_activities/rock_paper_scissors.py
@@ -1,6 +1,7 @@
 import random
-import colorama 
+import colorama
 from colorama import Fore, Style, init
+
 init(autoreset=True)
 
 def play_game():
@@ -9,9 +10,14 @@ def play_game():
         print(Fore.MAGENTA + "🎮 ROCK • PAPER • SCISSORS 🎮".center(60))
         print(Fore.CYAN + "=" * 60)
 
+        actions = ["rock", "paper", "scissors"]
+
         user = input(Fore.YELLOW + "👉 Enter a choice (rock, paper, scissors): " + Style.RESET_ALL)
 
-        actions = ["rock", "paper", "scissors"]
+        if user != "rock" and user != "paper" and user != "scissors":
+            print(Fore.RED + "⚠ Invalid input. Please choose rock, paper, or scissors." + Style.RESET_ALL)
+            continue  
+
         computer = random.choice(actions)
 
         print(Fore.BLUE + "🖥 Computer chose: " + Fore.MAGENTA + computer + Style.RESET_ALL)
@@ -37,12 +43,9 @@ def play_game():
         elif user == "scissors" and computer == "rock":
             print(Fore.RED + "💥 Rock defeats scissors! You lose 😢" + Style.RESET_ALL)
 
-        else:
-            print(Fore.RED + "⚠ Invalid input. Please choose rock, paper, or scissors." + Style.RESET_ALL)
-
         replay = input(Fore.MAGENTA + "🔁 Play again? (yes/no): " + Style.RESET_ALL)
 
-        if replay.lower() == "yes":
+        if replay == "yes":
             continue
         else:
             print(Fore.BLUE + "👋 Thanks for playing! Goodbye!" + Style.RESET_ALL)
